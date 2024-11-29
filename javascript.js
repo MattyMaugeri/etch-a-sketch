21989c820933a8d555e91a1808a89fac47d39b9f
Matthew Maugeri <53546314+MattyMaugeri@users.noreply.github.com>
2024-11-29 13:13:32 +1100

    Created button to delete old grid and make new grid using user input

--- javascript.js
+++ javascript.js
@@ -1,12 +1,13 @@
 const container = document.querySelector('#container');
+const newGridButton = document.querySelector('.new-grid');
 
-function grid() {
-    for (let i = 0; i < 16; i++) {
+function grid(newSize) {
+    for (let i = 0; i < newSize; i++) {
         const rowDiv = document.createElement('div');
         rowDiv.addEventListener('mouseover', hoverColor);
         rowDiv.classList.add('rowCell');
         container.appendChild(rowDiv);
-        for (let k = 0; k < 15; k++) {
+        for (let k = 0; k < newSize; k++) {
             const columnDiv = document.createElement('div');
             columnDiv.classList.add('columnCell');
             rowDiv.appendChild(columnDiv);
@@ -14,8 +15,15 @@ function grid() {
     }
 }
 
+function removeGrid(oldSize) {
+    for (let i = 0; i < oldSize; i++) {
+        const rowDiv = document.querySelector('.rowCell');
+        container.removeChild(rowDiv);
+    }
+}
+
 function randomColor() {
-    let randomColor = Math.floor(Math.random()*16777215).toString(16);
+    let randomColor = Math.floor(Math.random() * 16777215).toString(16);
     return '#' + randomColor;
 }
 
@@ -24,5 +32,23 @@ function hoverColor($event) {
     item.style.backgroundColor = randomColor();
 }
 
+let newGrid = 0;
+let oldGrid = 0;
 
-grid();
+newGridButton.addEventListener('click', () => {
+    let children = container.childElementCount;
+    newGrid = parseInt(Number(prompt('How many squares per side?')));    
+    
+    if ((newGrid > 0) && (newGrid < 100)) {
+        if (children == 0) {
+            grid(newGrid);
+            oldGrid = newGrid;
+        } else {
+            removeGrid(oldGrid);
+            grid(newGrid);
+            oldGrid = newGrid;
+        }
+    } else {
+        alert('Please enter a valid number between 0 and 100');
+    }
+});
